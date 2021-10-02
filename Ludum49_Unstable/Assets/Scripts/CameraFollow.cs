63b0510b3ec3e9bbe0b7fba3a7763c4a58b10842
BaptisteHollebecq <55829266+BaptisteHollebecq@users.noreply.github.com>
2021-10-02 06:52:46 +0200

    premier push

---
+++ Ludum49_Unstable/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform Ship;
+    public bool Rigid = false;
+
+    Vector3 _offeset = Vector3.zero;
+    float _distance = 0;
+    float _height = 0;
+
+    private void Awake()
+    {
+        _offeset = (transform.position - Ship.position).normalized;
+        _distance = (Ship.position - transform.position).magnitude;
+        _height = transform.position.y;
+    }
+
+    private void LateUpdate()
+    {
+        if (Rigid)
+            transform.position = Ship.position + (_offeset * _distance);
+        else
+        {
+            Vector3 newPos = Ship.position + (_offeset * _distance);
+            newPos.y = _height;
+            transform.position = newPos;
+        }
+    }
+}

8f0131cbce2f4fd7c7999ee0c96ddc655ccd3d83
rcq-rcq <55829293+rcq-rcq@users.noreply.github.com>
2021-10-04 18:20:25 +0200

    helice

---
+++ Ludum49_Unstable/Assets/RotateHelice.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateHelice : MonoBehaviour
+{
+    private Transform transform;
+    public float speed;
+    private float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform = GetComponent<Transform>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        y += Time.deltaTime * speed;
+        transform.localRotation = Quaternion.Euler(0, y, 0);
+    }
+}
